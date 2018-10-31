e9fe3ff9be471a75c16b40a4ebc31a233f962eeb
San Lee <sanlee2017@u.northwestern.edu>
2018-10-30 21:47:16 -0500

    strategy methods and test cases

--- santorini/santorini/Strategy.cs
+++ santorini/santorini/Strategy.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace santorini
@@ -16,12 +19,13 @@ namespace santorini
             return new List<dynamic>();
         }
 
-        public static List<dynamic> WinInOneTurn(Board board, string color)
+        //public static List<List<object>> WinInOneTurn(Board board, string color)
+        public static ArrayList WinInOneTurn(Board board, string color)
         {
             // get the coordinate location of the color players
             // check all eight directions of them to see if the height == 3
-            // if true, add the move direction to the directions[] and add any build direction
-            // append { worker, { move_dir, build_dir } }
+            // if true, add the move direction to the directions[] -- one element case
+            // append { worker, { move_dir } }
 
             List<string> targets = new List<string>(); // { "White1", White2" }
 
@@ -33,7 +37,7 @@ namespace santorini
                 }
             }
 
-            List<dynamic> OneTurnWinPlay = new List<dynamic>();
+            ArrayList OneTurnWinPlay = new ArrayList();
             foreach (var workerName in targets)
             {
                 foreach (var moveDir in board.directions.Keys)
@@ -43,11 +47,8 @@ namespace santorini
                         List<int> AfterMoveCoordinate = board.GetDesiredPosition(workerName, moveDir);
                         if (board.Board_[AfterMoveCoordinate[0], AfterMoveCoordinate[1]].Height == 3)
                         {
-                            // winning scenario
-                            foreach (var buildDir in board.directions.Keys)
-                            {
-                                OneTurnWinPlay.Add(new List<dynamic> { workerName, new List<dynamic> { moveDir, buildDir } });
-                            }
+                            // winning scenario with only moveDir
+                            OneTurnWinPlay.Add(new ArrayList { workerName, new ArrayList { moveDir } });
                         }
                     }
                 }
@@ -55,9 +56,124 @@ namespace santorini
             return OneTurnWinPlay;
         }
 
-        public static List<dynamic> PreventLoseInOneTurn(Board board, string oppColor)
+        public static ArrayList PreventLoseInOneTurn(Board board, string color, string oppColor)
         {
-            return new List<dynamic>();
+            /// 1. Run the WinInOneTurn for the oppWorkers
+            /// 3. calculate the targetCellPos (List<int>) by oppWorkerPos + moveDirection
+            /// 4. calculate the "path" from player worker to the targetCellPos by playerWorkerPos - targetCellPos
+            /// 5. in a for loop of all moveDirections, path - moveDir and lookup the List<int> key in Board.reverseDirection
+            ///      if containsKey, get the value of the key as buildDir and add { workerName, { moveDir, buildDir } }
+            ///      if not, continue
+            ArrayList validMoves = new ArrayList();
+            List<string> targets = new List<string>(); // { "myColor1", "myColor2" }
+
+            foreach (var workerName in board.PlayerPosition.Keys)
+            {
+                if (workerName.Contains(color))
+                {
+                    targets.Add(workerName);
+                }
+            }
+
+            ArrayList oppWorkerOneTurnWin = WinInOneTurn(board, oppColor);
+            if (oppWorkerOneTurnWin.Count != 0)
+            {
+                foreach (ArrayList oppPlay in oppWorkerOneTurnWin)
+                {
+                    // debug
+                    string JSONresult = JsonConvert.SerializeObject(oppPlay);
+                    Console.WriteLine(JSONresult);
+                    // parsing
+                    var worker = oppPlay[0].ToString();
+                    var moveDir = ((ArrayList)oppPlay[1])[0].ToString();
+                    //Console.WriteLine(worker + "-----");
+                    //Console.WriteLine(moveDir + "-----");
+                    List<int> targetCellPos = board.GetDesiredPosition(worker, moveDir);
+                    Console.WriteLine("targetCellPos: ");
+                    targetCellPos.ForEach(Console.WriteLine);
+
+
+                    foreach (var workerName in targets)
+                    {
+                        List<int> path = GetPossiblePath(board, workerName, targetCellPos);
+                        Console.WriteLine("path: ");
+                        path.ForEach(Console.WriteLine);
+                        if (path.Count != 0)
+                        {
+                            foreach (var item in CanExecutePathInOnePlay(board, workerName, path))
+                            {
+                                validMoves.Add(new ArrayList { workerName, item });
+                            }
+                        }
+                        Console.WriteLine("----END for one player worker----");
+                    }
+                }
+            }
+            return validMoves;
+        }
+
+        // return - list of possible paths: List<List<int>>
+        public static List<int> GetPossiblePath(Board board, string worker, List<int> targetCellPos)
+        {
+            Console.WriteLine("in GetPossiblePath");
+            List<int> path = new List<int>();
+            double MAX_DISTANCE_FOR_ONE_PLAY = 8.0;
+            double distance = 0;
+            List<int> workerPos = board.PlayerPosition[worker];
+            // calculate path (distance)
+            for (int i = 0; i < workerPos.Count; i++)
+            {
+                path.Add(targetCellPos[i] - workerPos[i]);
+                distance += Math.Pow(path[i], 2);
+            }
+            if (Math.Sqrt(distance) > Math.Sqrt(MAX_DISTANCE_FOR_ONE_PLAY))
+            {
+                Console.WriteLine("quitting cuz too far");
+                return new List<int> { };
+            }
+            return path;
+
+        }
+
+        // adding 10/30/2018 for Strategy class
+        // assumes that the coordinate will be not out of bounds
+        public static ArrayList CanExecutePathInOnePlay(Board board, string workerName, List<int> path)
+        {
+            Console.WriteLine("CanExecutePathInOnePlay:");
+            ArrayList result = new ArrayList();
+            foreach (var moveDir in board.directions.Keys)
+            {
+                List<int> key = new List<int>();
+                for (int i = 0; i < path.Count; i++)
+                {
+                    key.Add(path[i] - board.directions[moveDir][i]);
+
+                }
+                // need to take care of out of bound
+                if (!board.NeighboringCellExists(workerName, moveDir)) continue;
+
+                foreach (string buildDir in board.directions.Keys)
+                {
+                    if (key.SequenceEqual(board.directions[buildDir]))
+                    {
+                        Console.WriteLine("in arraylist equals:");
+                        Console.WriteLine("key:");
+                        key.ForEach(Console.WriteLine);
+                        Console.WriteLine("buildDir:");
+                        result.Add(new ArrayList { moveDir, buildDir });
+                    }
+                }
+
+                // List<> or ArrayList doesn't override Equals -- question
+                //if (board.reverseDirections.ContainsKey(key))
+                //{
+                //    Console.WriteLine("contains key");
+                //    string buildDir = board.reverseDirections[key];
+                //    result.Add(new List<string> { moveDir, buildDir });
+                //}
+            }
+
+            return result;
         }
     }
 }
--- santorini/santorini/tests/StrategyTest.cs
+++ santorini/santorini/tests/StrategyTest.cs
@@ -1,7 +1,10 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace santorini.tests
 {
@@ -22,26 +25,14 @@ namespace santorini.tests
             Board newBoard = new Board(JArray.Parse(validBoard));
             Player newPlayer = new Player();
 
-            List<dynamic> answer = new List<dynamic>
+            ArrayList answer = new ArrayList
             {
-                new List<dynamic> { "White1", new List<dynamic> {"E", "S"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "E"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "N"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "W"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "NW"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "SW"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "NE"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "SE"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "S"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "E"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "N"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "W"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "NW"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "SW"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "NE"} },
-                new List<dynamic> { "White1", new List<dynamic> {"S", "SE"} }
+                new ArrayList { "White1", new ArrayList { "E" } },
+                new ArrayList { "White1", new ArrayList { "S" } }
             };
-            CollectionAssert.AreEquivalent(Strategy.WinInOneTurn(newBoard, "White"), answer);
+            //string JSONresult = JsonConvert.SerializeObject(answer);
+            //Console.WriteLine(JSONresult);
+            CollectionAssert.AreEquivalent(answer, Strategy.WinInOneTurn(newBoard, "White"));
         }
 
 
@@ -59,76 +50,125 @@ namespace santorini.tests
             Board newBoard = new Board(JArray.Parse(validBoard));
             Player newPlayer = new Player();
 
-            List<dynamic> answer = new List<dynamic>
+
+            ArrayList answer = new ArrayList
             {
-                new List<dynamic> { "White1", new List<dynamic> {"E", "S"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "E"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "N"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "W"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "NW"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "SW"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "NE"} },
-                new List<dynamic> { "White1", new List<dynamic> {"E", "SE"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "S"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "E"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "N"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "W"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "NW"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "SW"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "NE"} },
-                new List<dynamic> { "White2", new List<dynamic> {"N", "SE"} }
+                new ArrayList { "White2", new ArrayList { "N" } },
+                new ArrayList { "White1", new ArrayList { "E" } }
+
             };
-            CollectionAssert.AreEquivalent(Strategy.WinInOneTurn(newBoard, "White"), answer);
+            CollectionAssert.AreEquivalent(answer, Strategy.WinInOneTurn(newBoard, "White"));
         }
 
-        //[Test()]
-        //// 3. a workers can prevent lose in one move by building a level 4
-        //public void TestBlue1PreventLoseInOne()
-        //{
-        //    string validBoard = @"[
-        //                        [[0, 'White1'], 3, 0, [0, 'Blue1'], 0]," +
-        //                        "[1, 0, 0, 0, 0]," +
-        //                        "[0, 0, 0, 0, 0]," +
-        //                        "[3, 0, 0, 0, 0]," +
-        //                        "[[0, 'White2'], 2, 0, 0, [0, 'Blue2']]" +
-        //                         "]";
-        //    Board newBoard = new Board(JArray.Parse(validBoard));
-        //    Player newPlayer = new Player();
+        [Test()]
+        // 3. a workers can prevent lose in one move by building a level 4
+        // BlueForce: Blue, OppForce: White
+        public void TestBlue1PreventLoseInOne()
+        {
+            string validBoard = @"[
+                                [[0, 'White1'], 3, 0, [0, 'Blue1'], 0]," +
+                                "[1, 0, 0, 0, 0]," +
+                                "[0, 0, 0, 0, 0]," +
+                                "[3, 0, 0, 0, 0]," +
+                                "[[0, 'White2'], 2, 0, 0, [0, 'Blue2']]" +
+                                 "]";
+            Board newBoard = new Board(JArray.Parse(validBoard));
+            Player newPlayer = new Player();
 
-        //    List<dynamic> answer = new List<dynamic>
-        //    {
-        //        new List<dynamic> {"Blue1", new List<dynamic> {"E", "S"}}
-        //    };
-        //    CollectionAssert.AreEqual(Strategy.PreventLoseInOneTurn(newBoard, "White"), answer);
-        //}
+            //JArray answer2 = new JArray
+            //{
+            //    new JArray {"Blue1", new JArray { "W", "W" } }
+            //};
 
-        //[Test()]
-        //// 4. two workers can prevent lose in one move by building a level 4
-        //public void TestBlue1Blue2PreventLoseInOne()
-        //{
-        //    string validBoard = @"[
-        //                        [[0, 'White1'], 3, 0, [0, 'Blue1'], 0]," +
-        //                        "[1, 3, 0, 0, 0]," +
-        //                        "[0, 0, 0, 0, 0]," +
-        //                        "[[0, 'Blue2'], 0, 0, 0, 0]," +
-        //                        "[[0, 'White2'], 2, 0, 0, 0]" +
-        //                         "]";
-        //    Board newBoard = new Board(JArray.Parse(validBoard));
-        //    Player newPlayer = new Player();
+            ArrayList answer = new ArrayList
+            {
+                new ArrayList {"Blue1", new ArrayList { "W", "W" } },
+                new ArrayList {"Blue1", new ArrayList { "SW", "NW" } },
+            };
+            //string JSONresult = JsonConvert.SerializeObject(answer);
+            //Console.WriteLine(JSONresult);
+            CollectionAssert.AreEquivalent(answer, Strategy.PreventLoseInOneTurn(newBoard, "Blue", "White"));
+        }
 
-        //    List<dynamic> answer = new List<dynamic>
-        //    {
-        //        new List<dynamic> {"Blue1", new List<dynamic> {"W", "W"}},
-        //        new List<dynamic> {"Blue1", new List<dynamic> {"W", "SW"}},
-        //        new List<dynamic> {"Blue1", new List<dynamic> {"SW", "W"}},
-        //        new List<dynamic> {"Blue2", new List<dynamic> {"N", "NE"}},
-        //        new List<dynamic> {"Blue2", new List<dynamic> {"NE", "N"}}
-        //    };
-        //    CollectionAssert.AreEqual(Strategy.PreventLoseInOneTurn(newBoard, "White"), answer);
-        //}
+        [Test()]
+        // 4. two workers can prevent lose in one move by building a level 4
+        // BlueForce: Blue, OppForce: White
+        public void TestBlue1Blue2PreventLoseInOne()
+        {
+            string validBoard = @"[
+                                [[0, 'White1'], 3, 0, [0, 'Blue1'], 0]," +
+                                "[1, 3, 0, 0, 0]," +
+                                "[0, 0, 0, 0, 0]," +
+                                "[[0, 'Blue2'], 0, 0, 0, 0]," +
+                                "[[0, 'White2'], 2, 0, 0, 0]" +
+                                 "]";
+            Board newBoard = new Board(JArray.Parse(validBoard));
+            Player newPlayer = new Player();
+
+            ArrayList answer = new ArrayList
+            {
+                new ArrayList {"Blue1", new ArrayList { "W", "W"}},
+                new ArrayList {"Blue1", new ArrayList { "SW", "NW"}},
+                new ArrayList {"Blue1", new ArrayList { "W", "SW"}},
+                new ArrayList {"Blue1", new ArrayList { "SW", "W"}},
+                new ArrayList {"Blue2", new ArrayList {"N", "NE"}},
+                new ArrayList {"Blue2", new ArrayList { "NE", "N"}}
+            };
+            CollectionAssert.AreEquivalent(answer, Strategy.PreventLoseInOneTurn(newBoard, "Blue", "White"));
+        }
 
 
         // 2. the player can win in one move - one direction
         // 3. the player can block opponent from winning in one move - one direction
+
+        // helper functions test
+        [Test()]
+        // provide a path that a worker can go in one play(move and build)
+        public void TestGetPossiblePathValid()
+        {
+            string validBoard = @"[
+                                 [0, 2, [0, 'Blue1'], 0, 3]," +
+                                "[1, 0, 0, 0, 0]," +
+                                "[0, 0, 1, 0, 0]," +
+                                "[0, 0, 0, 1, 0]," +
+                                "[0, 2, [0, 'White1'], 0, 0]" +
+                                 "]";
+            Board newBoard = new Board(JArray.Parse(validBoard));
+            List<int> answer = new List<int> { 2, 0 };
+            //CollectionAssert.AreEquivalent(answer, Strategy.GetPossiblePath(newBoard, "Blue1", new List<int> { 2, 2 }));
+            List<int> value = Strategy.GetPossiblePath(newBoard, "Blue1", new List<int> { 2, 2 });
+            Assert.IsTrue(answer.SequenceEqual(value));
+
+            List<int> answer2 = new List<int> { -2, 0 };
+            List<int> value2 = Strategy.GetPossiblePath(newBoard, "White1", new List<int> { 2, 2 });
+            Assert.IsTrue(answer2.SequenceEqual(value2));
+
+           
+        }
+
+        [Test()]
+        // 
+        public void TestGetPossiblePathInvalid()
+        {
+            string validBoard = @"[
+                                 [0, 2, [0, 'Blue1'], 0, 3]," +
+                                "[1, 0, 0, 0, 0]," +
+                                "[0, 0, 1, 0, 0]," +
+                                "[0, 0, 0, 1, 0]," +
+                                "[0, 2, [0, 'White1'], 0, 0]" +
+                                 "]";
+            Board newBoard = new Board(JArray.Parse(validBoard));
+
+            List<int> answer3 = new List<int>();
+            List<int> value3 = Strategy.GetPossiblePath(newBoard, "White1", new List<int> { 0, 4 });
+            value3.ForEach(Console.WriteLine);
+            Assert.IsTrue(answer3.SequenceEqual(value3));
+
+            //List<int> answer4 = new List<int> { 2, -2 };
+            List<int> value4 = Strategy.GetPossiblePath(newBoard, "Blue1", new List<int> { 3, 0 });
+            Assert.IsTrue(answer3.SequenceEqual(value4));
+        }
+
+      
     }
 }
