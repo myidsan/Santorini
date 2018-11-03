21321ef28bfbd662e2707fde60d82327a06321e4
San Lee <sanlee2017@u.northwestern.edu>
2018-11-03 04:12:52 -0500

    correct test harness for assign4

--- santorini/santorini/Program.cs
+++ santorini/santorini/Program.cs
@@ -35,89 +35,149 @@ namespace santorini
         /// Test harness for the rule checker
         /// </summary>
         /// <param name="args">The command-line arguments.</param>
-        //public static void Main(string[] args)
-        //{
-        //    JSONEncoder parser = new JSONEncoder();
-        //    //RuleChecker checker = new RuleChecker();
-        //    JToken input;
-
-        //    // dispatch -- reading from STDIN
-        //    while (true)
-        //    {
-        //        input = parser.JSONParser();
-        //        if (input == null) break;
-        //        // validate board
-        //        Board newBoard = new Board((JArray)input[0]);
-        //        Cell[,] orignalBoard = newBoard.Board_;
-
-        //        Board tempBoardObj;
-        //        Cell[,] tempBoard;
-
-        //        string JSONresult;
-
-        //        if (!RuleChecker.IsBoardValid(newBoard))
-        //        {
-        //            JSONresult = JsonConvert.SerializeObject("no");
-        //            Console.WriteLine(JSONresult);
-        //            continue;
-        //        }
-
-        //        // validate move
-        //        string worker = (string)input[1];
-        //        JArray directions = (JArray)input[2];
-        //        string[] stringDirections = directions.ToObject<string[]>();
-        //        if (!RuleChecker.IsValidMove(newBoard, worker, stringDirections))
-        //        {
-        //            JSONresult = JsonConvert.SerializeObject("no");
-        //            Console.WriteLine(JSONresult);
-        //            continue;
-        //        }
-
-        //        // make a tempBoard and 
-        //        // construct a new tempBoardObj for build
-        //        tempBoard = newBoard.Move(worker, stringDirections[0]);
-        //        tempBoardObj = new Board(tempBoard, newBoard.PlayerPosition);
-
-        //        // check whether the player has won
-        //        // if so, terminate the game
-        //        if (RuleChecker.IsPlayerWinner(tempBoardObj, worker))
-        //        {
-        //            Console.WriteLine("worker is on level 3: player wins");
-        //            continue;
-        //        }
-
-        //        // validate build
-        //        if (!RuleChecker.IsValidBuild(tempBoardObj, worker, stringDirections))
-        //        {
-        //            JSONresult = JsonConvert.SerializeObject("no");
-        //            Console.WriteLine(JSONresult);
-        //            continue;
-        //        }
-        //        tempBoard = tempBoardObj.Move(worker, stringDirections[0]);
-
-        //        // all good
-        //        JSONresult = JsonConvert.SerializeObject("yes");
-        //        Console.WriteLine(JSONresult);
-        //    }
-        //    return;
-        //}
         public static void Main(string[] args)
         {
             JSONEncoder parser = new JSONEncoder();
+            //RuleChecker checker = new RuleChecker();
+            JToken input;
 
-            JArray input = parser.JSONParser();
-            string color = input[1].ToString();
-            Player newPlayer = new Player(color);
-            newPlayer.RunCommand(input);
-
-            // dispatch
+            // dispatch -- reading from STDIN
             while (true)
             {
                 input = parser.JSONParser();
                 if (input == null) break;
-                newPlayer.RunCommand(input);
+                // validate board
+                Board newBoard = new Board((JArray)input[0]);
+
+                Board tempBoardObj;
+                Cell[,] tempBoard;
+
+                string JSONresult;
+
+                if (!RuleChecker.IsBoardValid(newBoard))
+                {
+                    JSONresult = JsonConvert.SerializeObject("no");
+                    Console.WriteLine(JSONresult);
+                    continue;
+                }
+
+                // validate move
+                string worker = (string)input[1];
+                JArray directions = (JArray)input[2];
+                string[] stringDirections = directions.ToObject<string[]>();
+
+                if (stringDirections.Length == 1)
+                {
+                    if (!RuleChecker.IsValidMove(newBoard, worker, stringDirections))
+                    {
+                        JSONEncoder.PrintJson("no");
+                        continue;
+                    }
+                    newBoard.Move(worker, stringDirections[0]);
+                    if (!RuleChecker.IsPlayerWinner(newBoard, worker))
+                    {
+                        JSONEncoder.PrintJson("no");
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (!RuleChecker.IsValidMove(newBoard, worker, stringDirections))
+                    {
+                        JSONEncoder.PrintJson("no");
+                        continue;
+                    }
+                    tempBoard = newBoard.Move(worker, stringDirections[0]);
+                    JArray test = new JArray();
+                    test = newBoard.DumpBoard();
+                    tempBoardObj = new Board(test);
+                    if (!RuleChecker.IsValidBuild(tempBoardObj, worker, stringDirections))
+                    {
+                        JSONEncoder.PrintJson("no");
+                        continue;
+                    }
+                }
+                JSONEncoder.PrintJson("yes");
+
+
+                //    if (stringDirections.Length == 1)
+                //    {
+                //        if (RuleChecker.IsValidMove(newBoard, worker, stringDirections))
+                //        {
+                //            tempBoard = newBoard.Move(worker, stringDirections[0]);
+                //            if (RuleChecker.IsPlayerWinner(newBoard, worker))
+                //            {
+                //                JSONresult = JsonConvert.SerializeObject("yes");
+                //                Console.WriteLine(JSONresult);
+                //                continue;
+                //            }
+                //            else
+                //            {
+                //                JSONresult = JsonConvert.SerializeObject("no");
+                //                Console.WriteLine(JSONresult);
+                //                continue;
+                //            }
+                //        }
+                //        else
+                //        {
+                //            JSONresult = JsonConvert.SerializeObject("no");
+                //            Console.WriteLine(JSONresult);
+                //            continue;
+                //        }
+                //    }
+                //    else
+                //    {
+                //        // length == 2 and not valid move
+                //        if (!RuleChecker.IsValidMove(newBoard, worker, stringDirections))
+                //        {
+                //            JSONEncoder.PrintJson("no");
+                //            continue;
+                //        }
+
+                //        // valid move, check valid move with a new tempBoard
+                //        tempBoard = newBoard.Move(worker, stringDirections[0]);
+                //        // make a tempBoard and move
+                //        JArray test = new JArray();
+                //        test = newBoard.DumpBoard();
+                //        tempBoardObj = new Board(test);
+                //        tempBoard = tempBoardObj.Move(worker, stringDirections[0]);
+
+                //        if (!RuleChecker.IsValidBuild(tempBoardObj, worker, stringDirections))
+                //        {
+                //            JSONresult = JsonConvert.SerializeObject("no");
+                //            Console.WriteLine(JSONresult);
+                //            continue;
+                //        }
+
+                //        // all good
+                //        JSONresult = JsonConvert.SerializeObject("yes");
+                //        Console.WriteLine(JSONresult);
+                //    }
+
+                //}
+                //return;
+                //}
+
+                //////// assignment 5
+                //public static void Main(string[] args)
+                //{
+                //    JSONEncoder parser = new JSONEncoder();
+
+                //    JArray input = parser.JSONParser();
+                //    string color = input[1].ToString();
+                //    Player newPlayer = new Player(color);
+                //    newPlayer.RunCommand(input);
+
+                //    // dispatch
+                //    while (true)
+                //    {
+                //        input = parser.JSONParser();
+                //        if (input == null) break;
+                //        newPlayer.RunCommand(input);
+                //    }
+                //    return;
+                //}
             }
-            return;
         }
     }
 }
